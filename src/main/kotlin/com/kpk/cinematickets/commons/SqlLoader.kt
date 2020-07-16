f2f934cd47877a5c59ff148c69a37c58e4464724
Anton <anton.naumov@nevexis.com>
2020-07-16 20:21:07 +0300

    on application start load database queries from .sql files into SqlLoader static properties

---
+++ src/main/kotlin/com/kpk/cinematickets/commons/SqlLoader.kt
@@ -0,0 +1,35 @@
+package com.kpk.cinematickets.commons
+
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.lang.invoke.MethodHandles
+
+@Component
+class SqlLoader(@Value("\${sql.container}") sqlFilesPath: String) {
+
+    private val logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().canonicalName)
+
+    companion object Queries {
+    }
+
+    init {
+        SqlLoader::class.java.fields
+                .filter { it.name != "Queries" }
+                .forEach {
+                    it.isAccessible = true
+                    val fieldValue = getSQLStatement(sqlFilesPath, it.name.toLowerCase() + ".sql")
+                    it.set(this, fieldValue)
+                }
+    }
+
+    private fun getSQLStatement(sqlPath: String, sqlFileName: String): String {
+        try {
+            val filePath = "/$sqlPath/$sqlFileName"
+            return this::class.java.getResource(filePath).readText(Charsets.UTF_8)
+        } catch (e: Exception) {
+            logger.info("Missing file: $sqlPath $sqlFileName")
+            throw e
+        }
+    }
+}
