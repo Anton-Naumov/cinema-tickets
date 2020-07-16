5d9f91a28abbc37d73afcad5036b0e750e410b82
Anton <anton.naumov@nevexis.com>
2020-07-16 16:13:23 +0300

    refactor oauth2 config to use @Value annotation for application properties

--- src/main/kotlin/com/kpk/cinematickets/config/Oauth2Config.kt
+++ src/main/kotlin/com/kpk/cinematickets/config/Oauth2Config.kt
@@ -1,8 +1,8 @@
 package com.kpk.cinematickets.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
 import org.springframework.jdbc.core.JdbcOperations
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider
 import org.springframework.security.oauth2.client.JdbcOAuth2AuthorizedClientService
@@ -11,17 +11,14 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
 
 @Configuration
-class Oauth2Config(val env: Environment) {
-    companion object {
-        private const val CLIENT = "google"
-        private const val CLIENT_PROPERTY_KEY = "spring.security.oauth2.client.registration"
-    }
+class Oauth2Config(
+        @Value("\${spring.security.oauth2.client.registration.google.client-id}") val clientId: String,
+        @Value("\${spring.security.oauth2.client.registration.google.client-secret}") val clientSecret: String
+) {
 
     @Bean
     fun clientRegistrationRepository(): ClientRegistrationRepository {
-        val clientId: String = env.getProperty("$CLIENT_PROPERTY_KEY.$CLIENT.client-id") ?: throw Exception("")
-        val clientSecret: String = env.getProperty("$CLIENT_PROPERTY_KEY.$CLIENT.client-secret") ?: throw Exception("")
-        val registration= CommonOAuth2Provider.GOOGLE.getBuilder(CLIENT)
+        val registration= CommonOAuth2Provider.GOOGLE.getBuilder("google")
                 .clientId(clientId).clientSecret(clientSecret).build()
         return InMemoryClientRegistrationRepository(registration)
     }
@@ -30,4 +27,5 @@ class Oauth2Config(val env: Environment) {
     fun authorizedClientService(jdbcOperations: JdbcOperations, clientRegistrationRepository: ClientRegistrationRepository): OAuth2AuthorizedClientService {
         return JdbcOAuth2AuthorizedClientService(jdbcOperations, clientRegistrationRepository)
     }
+
 }
